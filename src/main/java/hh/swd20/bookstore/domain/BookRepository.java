30f8883f971084f0844663dad77b1c14a4efe18b
Sofia Lumme <sofia.lumme@gmail.com>
2022-03-08 17:16:22 +0200

    added auto-generated Spring Data REST

--- src/main/java/hh/swd20/bookstore/domain/BookRepository.java
+++ src/main/java/hh/swd20/bookstore/domain/BookRepository.java
@@ -1,7 +1,14 @@
 package hh.swd20.bookstore.domain;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long> {
+	
+	List<Book> findByYear(@Param("year") int year);
 
 }
